40b8b1d14d686e9d72b8ee0780caaca99470f373
Nicholas Hama <njhama@usc.edu>
2023-07-10 15:33:28 -0700

    changed some comments

--- sherlock/sherlock.py
+++ sherlock/sherlock.py
@@ -33,42 +33,14 @@ __version__ = "0.14.3"
 
 class SherlockFuturesSession(FuturesSession):
     def request(self, method, url, hooks=None, *args, **kwargs):
-        """Request URL.
 
-        This extends the FuturesSession request method to calculate a response
-        time metric to each request.
-
-        It is taken (almost) directly from the following Stack Overflow answer:
-        https://github.com/ross/requests-futures#working-in-the-background
-
-        Keyword Arguments:
-        self                   -- This object.
-        method                 -- String containing method desired for request.
-        url                    -- String containing URL for request.
-        hooks                  -- Dictionary containing hooks to execute after
-                                  request finishes.
-        args                   -- Arguments.
-        kwargs                 -- Keyword arguments.
-
-        Return Value:
-        Request object.
-        """
         # Record the start time for the request.
         if hooks is None:
             hooks = {}
         start = monotonic()
 
         def response_time(resp, *args, **kwargs):
-            """Response Time Hook.
 
-            Keyword Arguments:
-            resp                   -- Response object.
-            args                   -- Arguments.
-            kwargs                 -- Keyword arguments.
-
-            Return Value:
-            Nothing.
-            """
             resp.elapsed = monotonic() - start
 
             return
@@ -163,36 +135,7 @@ def MultipleUsernames(username):
 def sherlock(username, site_data, query_notify,
              tor=False, unique_tor=False,
              proxy=None, timeout=60):
-    """Run Sherlock Analysis.
 
-    Checks for existence of username on various social media sites.
-
-    Keyword Arguments:
-    username               -- String indicating username that report
-                              should be created against.
-    site_data              -- Dictionary containing all of the site data.
-    query_notify           -- Object with base type of QueryNotify().
-                              This will be used to notify the caller about
-                              query results.
-    tor                    -- Boolean indicating whether to use a tor circuit for the requests.
-    unique_tor             -- Boolean indicating whether to use a new tor circuit for each request.
-    proxy                  -- String indicating the proxy URL
-    timeout                -- Time in seconds to wait before timing out request.
-                              Default is 60 seconds.
-
-    Return Value:
-    Dictionary containing results from report. Key of dictionary is the name
-    of the social network site, and the value is another dictionary with
-    the following keys:
-        url_main:      URL of main site.
-        url_user:      URL of user on site (if account exists).
-        status:        QueryResult() object indicating results of test for
-                       account existence.
-        http_status:   HTTP status code of query which checked for existence on
-                       site.
-        response_text: Text that came back from request.  May be None if
-                       there was an HTTP error when checking for existence.
-    """
 
     # Notify caller that we are starting the query.
     query_notify.start(username)
